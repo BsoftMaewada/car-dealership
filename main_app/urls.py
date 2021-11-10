ba6fb6414018235298692f8ff17e8ea5ba4560c3
Buhari Nasir Maiwada <maewada@live.com>
2021-11-09 23:07:52 -0500

    added about.html, contact.html and services.html files

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # navbars urls
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contacts', views.contact, name='contact'),
+   
     
 ]
