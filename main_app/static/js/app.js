82486ae1e4f66e73506d966c5c38c50c3660db49
Buhari Nasir Maiwada <maewada@live.com>
2021-11-10 00:33:20 -0500

    modified base, contact, about and services.htmls

--- main_app/static/js/app.js
+++ main_app/static/js/app.js
@@ -240,7 +240,7 @@ $(function () {
     $( function() {
         // Add background image
         //$.backstretch('../img/nature.jpg');
-        var endDate = "December  27, 2019 15:03:25";
+        var endDate = "December  27, 2021 15:03:25";
         $('.countdown.simple').countdown({ date: endDate });
         $('.countdown.styled').countdown({
             date: endDate,
