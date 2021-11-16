cee5ffb26cbef7c47d70f84720bf2ea9fd1ff712
Buhari Nasir Maiwada <maewada@live.com>
2021-11-15 23:36:33 -0500

    added car app and its models

--- cars/models.py
+++ cars/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from datetime import datetime
+from ckeditor.fields import RichTextField
+from multiselectfield import MultiSelectField
 
 # Create your models here.
 #car_title
@@ -79,30 +82,62 @@ class Car(models.Model):
         ('WI', 'Wisconsin'),
         ('WY', 'Wyoming'),
     )
+    # To auto populate the year from 2021 above
+    year_choice =[]
+    for i in range(2000, (datetime.now().year + 1)):
+        year_choice.append((i, i))
+        
+        features_choices = (
+        ('Cruise Control', 'Cruise Control'),
+        ('Audio Interface', 'Audio Interface'),
+        ('Airbags', 'Airbags'),
+        ('Air Conditioning', 'Air Conditioning'),
+        ('Seat Heating', 'Seat Heating'),
+        ('Alarm System', 'Alarm System'),
+        ('ParkAssist', 'ParkAssist'),
+        ('Power Steering', 'Power Steering'),
+        ('Reversing Camera', 'Reversing Camera'),
+        ('Direct Fuel Injection', 'Direct Fuel Injection'),
+        ('Auto Start/Stop', 'Auto Start/Stop'),
+        ('Wind Deflector', 'Wind Deflector'),
+        ('Bluetooth Handset', 'Bluetooth Handset'),
+    )
+    
+    door_choices = (
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('6', '6'),
+    )
+    
     car_title = models.CharField(max_length=255)
-    state = models.CharField(max_length=255)
+    state = models.CharField(choices=state_choices, max_length=255)
     city = models.CharField(max_length=255)
     color = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
-    year = models.CharField(max_length=255)
+    year = models.IntegerField(('year'), choices = year_choice)
     condition = models.CharField(max_length=255)
-    price = models.CharField(max_length=255)
-    car_photo = models.CharField(max_length=255)
-    car_photo_1 = models.CharField(max_length=255)
-    car_photo_2 = models.CharField(max_length=255)
-    car_photo_3 = models.CharField(max_length=255)
-    car_photo_4 = models.CharField(max_length=255)
-    car_photo_5 = models.CharField(max_length=255)
-    features = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = RichTextField()
+    car_photo = models.ImageField(upload_to='photo/%Y/%m/%d/')
+    car_photo_1 = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
+    car_photo_2 = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
+    car_photo_3 = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
+    car_photo_4 = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
+    car_photo_5 = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
+    features = MultiSelectField(choices=features_choices)
     body_style = models.CharField(max_length=255)
     engine = models.CharField(max_length=255)
     transmission = models.CharField(max_length=255)
     interior = models.CharField(max_length=255)
-    mileage = models.CharField(max_length=255)
-    fuel_type = models.CharField(max_length=255)
+    mileage = models.IntegerField()
+    fuel_type = models.CharField(max_length=50)
+    doors = models.CharField(choices=door_choices, max_length=10)
     no_of_owners = models.CharField(max_length=255)
-    is_feature = models.CharField(max_length=255)
-    created_date = models.CharField(max_length=255)
+    is_feature = models.BooleanField(default=False)
+    vin = models.CharField(max_length=255)
+    created_date = models.DateField(default=datetime.now, blank=True)
     
 
 
